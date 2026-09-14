bc9584c614117109bc9429f0ed12f359c0ced19b
jalajknimesh <jalajkumar1011@gmail.com>
2026-09-14 12:07:23 +0530

    Preserve enhancement script order

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ if (yearNode) yearNode.textContent = new Date().getFullYear();
 ['home-enhance.js','motion.js'].forEach((src) => {
   const script = document.createElement('script');
   script.src = src;
-  script.defer = true;
+  script.async = false;
   document.head.appendChild(script);
 });
